6265b560f71be496b7d9548bd31889898ff270f7
ThriftOdysseyHub <thriftodysseyhub@gmail.com>
2025-07-29 15:39:59 +0000

    1

---
+++ data/products.js
@@ -0,0 +1,60 @@
+/**
+ * Product Data for ThriftOdysseyHub
+ * * This file contains the list of products to be displayed on the e-commerce site.
+ * Each product is an object with the following properties:
+ * - id: A unique number to identify the product.
+ * - name: The name of the product (e.g., 'Vintage Denim Jacket').
+ * - price: The price of the product in a numeric format (e.g., 58.00).
+ * - image: The URL of the product image.
+ */
+
+const products = [
+    { 
+        id: 1, 
+        name: 'Vintage Denim Jacket', 
+        price: 58.00, 
+        image: 'https://images.unsplash.com/photo-1543087903-1ac2ec7aa8c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' 
+    },
+    { 
+        id: 2, 
+        name: 'Canvas Messenger Bag', 
+        price: 65.00, 
+        image: 'https://images.unsplash.com/photo-1547949003-9792a18a2601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' 
+    },
+    { 
+        id: 3, 
+        name: 'Bohemian Floral Dress', 
+        price: 42.50, 
+        image: 'https://images.unsplash.com/photo-1596904259845-a73496b48439?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' 
+    },
+    { 
+        id: 4, 
+        name: 'Retro Aviator Sunglasses', 
+        price: 32.00, 
+        image: 'https://images.unsplash.com/photo-1577803645773-40491993313a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1008&q=80' 
+    },
+    { 
+        id: 5, 
+        name: 'Wool Blend Peacoat', 
+        price: 85.00, 
+        image: 'https://images.unsplash.com/photo-1605860783325-035370f30374?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80' 
+    },
+    { 
+        id: 6, 
+        name: 'Ceramic Trinket Dish', 
+        price: 24.00, 
+        image: 'https://images.unsplash.com/photo-1616016970794-8a7e37d680c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' 
+    },
+    { 
+        id: 7, 
+        name: 'Wrangler Corduroy Trousers', 
+        price: 48.00, 
+        image: 'https://images.unsplash.com/photo-1604176354204-926873782855?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' 
+    },
+    { 
+        id: 8, 
+        name: 'Italian Silk Scarf', 
+        price: 29.00, 
+        image: 'https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1049&q=80' 
+    },
+];
